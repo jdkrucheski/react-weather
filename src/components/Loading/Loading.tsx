550c5fb00ea5a66a92f1a698c7fcee77994722a1
Jonathan David Krucheski <jonathankrucheski@gmail.com>
2022-08-30 12:37:37 -0300

    create login component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,10 @@
+import "./loading.css";
+export const Loading = () => {
+  return (
+    <div className="loader">
+      <svg className="circularLoader" viewBox="25 25 50 50">
+        <circle className="loaderPath" cx="50" cy="50" r="20" fill="none"></circle>
+      </svg>
+    </div>
+  );
+};
